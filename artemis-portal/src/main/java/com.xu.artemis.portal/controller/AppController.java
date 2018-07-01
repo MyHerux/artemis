3fee1db48bd19e97a6ba3ffb94cb13bdc3afb4ae
hua.xu <hua.xu@bitmain.com>
2018-07-01 18:40:45 +0800

    add travis pom support

--- artemis-portal/src/main/java/com.xu.artemis.portal/controller/AppController.java
+++ artemis-portal/src/main/java/com.xu.artemis.portal/controller/AppController.java
@@ -4,28 +4,33 @@ import com.xu.artemis.portal.entities.App;
 import com.xu.artemis.portal.repository.AppRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
 
 @RestController
 @RequestMapping("/apps")
 public class AppController {
 
-  @Autowired
-  private AppRepository appRepository;
+    @Autowired
+    private AppRepository appRepository;
 
-  @RequestMapping("")
-  public Page<App> list() {
-    Pageable pageable = new PageRequest(0, 10);
-    return appRepository.findAll(pageable);
-  }
+    @RequestMapping("")
+    public Page<App> list(@PageableDefault(size = 50) Pageable pageable) {
+        return appRepository.findAll(pageable);
+    }
 
-  @RequestMapping(value = "", method = RequestMethod.POST)
-  public App create() {
-    App ramdomApp = new App();
-    return appRepository.save(ramdomApp);
-  }
+    @RequestMapping(value = "", method = RequestMethod.POST)
+    public App create() {
+        App ramdomApp = new App();
+        ramdomApp.setId(String.valueOf(System.currentTimeMillis()));
+        ramdomApp.setName("new app " + System.currentTimeMillis());
+        ramdomApp.setOwner("owner " + System.currentTimeMillis());
+        ramdomApp.setCreateTimestamp(new Date());
+        return appRepository.save(ramdomApp);
+    }
 }
