678bcbc36569d6917164b36d9fbc44ea1ffbce83
hua.xu <hua.xu@bitmain.com>
2018-07-02 22:52:44 +0800

    add artemis-biz

---
+++ artemis-biz/src/main/java/com/xu/artemis/biz/ArtemisBizApplication.java
@@ -0,0 +1,12 @@
+package com.xu.artemis.biz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtemisBizApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArtemisBizApplication.class, args);
+    }
+}
