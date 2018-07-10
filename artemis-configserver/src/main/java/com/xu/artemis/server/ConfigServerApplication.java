804a9cbf87a599f1b070e1c1ca4390c2b2fdc396
myherux <1013299500@qq.com>
2018-07-10 20:40:29 +0800

    fix: test

--- artemis-configserver/src/main/java/com/xu/artemis/server/ConfigServerApplication.java
+++ artemis-configserver/src/main/java/com/xu/artemis/server/ConfigServerApplication.java
@@ -1,14 +1,10 @@
 package com.xu.artemis.server;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
-@Configuration
-@EnableAutoConfiguration
 @EnableConfigServer
 public class ConfigServerApplication {
 
