14355696623cd566fe96ce04e4140872c461bd8b
myherux <1013299500@qq.com>
2018-07-11 17:58:24 +0800

    使用Thymeleaf替代jsp

--- artemis-demo/src/main/java/com.xu.artemis.demo/web/DemoController.java
+++ artemis-demo/src/main/java/com.xu.artemis.demo/web/DemoController.java
@@ -3,12 +3,11 @@ package com.xu.artemis.demo.web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
-@RestController
+@Controller
 @RequestMapping("/demo")
 public class DemoController {
 
@@ -18,7 +17,13 @@ public class DemoController {
     @Value("${apollo.foo}")
     private String foo;
 
-    @RequestMapping(value = "/config/{configName:.*}", method = RequestMethod.GET)
+    @RequestMapping(value = "welcome", method = RequestMethod.GET)
+    public  String  welcome(){
+        return "welcome";
+    }
+
+    @GetMapping(value = "/config/{configName:.*}")
+    @ResponseBody
     public String queryConfig(@PathVariable String configName) {
         return env.getProperty(configName, "undefined");
     }
